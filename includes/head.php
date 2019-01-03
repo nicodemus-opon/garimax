6d2029160676e608e0c6231e9505a5acf72b727d
Nicodemus Opon <nicodemusopon@gmail.com>
2019-01-03 15:50:56 +0300

    first commit

---
+++ includes/head.php
@@ -0,0 +1,27 @@
+
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Garimax</title>
+    <meta charset="utf-8" />
+<link rel="apple-touch-icon" sizes="76x76" href="./assets/img/apple-icon.png">
+<link rel="icon" type="image/png" href="./assets/img/favicon.png">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="" integrity="" crossorigin="anonymous">
+    <link href="./assets/css/bootstrap.min.css" rel="stylesheet" />
+    <!-- NOW UI Kit CSS -->
+    <link rel="stylesheet" href="./assets/css/now-ui-kit.min.css">
+
+</head>
+
+<body>
+    
+    
---
+++ index.php
@@ -0,0 +1,82 @@
+<?php
+include('includes/head.php');
+
+?>
+    <div class="header-2">
+        <nav class="navbar navbar-expand-lg navbar-transparent bg-primary navbar-absolute">
+            <div class="container">
+                <div class="navbar-translate">
+                    <button class="navbar-toggler" type="button" data-toggle="collapse"
+                            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-bar bar1"></span>
+                        <span class="navbar-toggler-bar bar2"></span>
+                        <span class="navbar-toggler-bar bar3"></span>
+                    </button>
+                    <a class="navbar-brand" href="#pablo">GariMax</a>
+                </div>
+
+                <div class="collapse navbar-collapse">
+                    <ul class="navbar-nav mx-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#pablo">
+                                Home
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#pablo">
+                                Login
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#pablo">
+                                Sign Up
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#pablo">
+                                Contact Me
+                            </a>
+                        </li>
+                    </ul>
+
+                    <ul class="nav navbar-nav navbar-right">
+                        <li class="nav-item">
+                            <a class="nav-link" href="https://twitter.com/CreativeTim">
+                                <i class="fab fa-twitter"></i>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="https://www.facebook.com/CreativeTim">
+                                <i class="fab fa-facebook-square"></i>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="https://www.instagram.com/CreativeTimOfficial">
+                                <i class="fab fa-instagram"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+
+        <div class="page-header header-filter">
+            <div class="page-header-image" style="background-image: url('./assets/img/bg2.jpg');"></div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 ml-auto mr-auto text-center">
+                        <h1 class="title"> Hello!</h1>
+                        <h5 class="description">The site is currently under development.</h5>
+                        <p>
+                            <a class="btn-primary btn-round btn " href="tel:0707584688"> Give us a Call</a>
+                        </p>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+<?php
+include("includes/end.php");
+?>
