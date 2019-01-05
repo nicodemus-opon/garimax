a0e0f07de294f11ce74898c1320ff1d0ee29a0f2
Nicodemus Opon <nicodemusopon@gmail.com>
2019-01-05 14:35:27 +0300

    jkk

--- includes/head.php
+++ includes/head.php
@@ -1,17 +1,47 @@
 <!doctype html>
 <html lang="en">
-
+<script type="application/ld+json">
+    {
+        "@context": "http://schema.org",
+        "@type": "WebSite",
+        "url": "https://garimax.co.ke",
+        "potentialAction":
+        { "@type": "SearchAction",
+            "target": "https://garimax.co.ke/search.php?q={search_term_string}",
+            "query-input": "required name=search_term_string" }
+    }
+</script>
 <head>
-    <title>Garimax</title>
+    <title>Garimax Motors Ltd</title>
     <meta charset="utf-8"/>
     <link rel="apple-touch-icon" sizes="76x76" href="./assets/img/apple-icon.png">
     <link rel="icon" type="image/png" href="./assets/img/favicon.png">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
     <!-- seo-->
-
+    <script type="application/ld+json">
+        {
+            "@context" : "http://schema.org",
+            "@type" : "LocalBusiness",
+            "name":"Garimax Motors LTD",
+            "priceRange":"50000-1.5M",
+            "image" : [ "https://garimax.co.ke/cars/1/i3.jpeg", "https://garimax.co.ke/cars/2/i9.jpeg", "https://garimax.co.ke/cars/3/i3.jpeg" ],
+            "telephone" : "0733626262",
+            "address" : {
+                "@type" : "PostalAddress",
+                "streetAddress" : "Fortis Industrial park"
+            },
+            "openingHoursSpecification" : {
+                "@type" : "OpeningHoursSpecification",
+                "dayOfWeek" : {
+                    "@type" : "DayOfWeek",
+                    "name" : "Mon - Fri, 8:00-22:00"
+                }
+            }
+        }
+    </script>
     <!--  Social tags      -->
     <meta name="keywords"
-          content="Garimax, Gari Max, Car dealership Kenya, used cars,cheap,quality">
+          content="Garimax, Gari Max, Car dealership Kenya, used cars,cheap,quality,garimax motors">
     <meta name="description"
           content="GariMax Motors ltd, imports and sells Quality Used cars from Australia, Britain and Japan. Any car needs? we are here at your service">
 
